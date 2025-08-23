703c947f647a6fc593a7b1a8cdea2a64ae087203
GustavoRu <gustavoruiz.gr96@gmail.com>
2025-08-23 17:39:50 -0300

    ajustes

--- tests/TestFiles/SampleClass.cs
+++ tests/TestFiles/SampleClass.cs
@@ -3,14 +3,14 @@ public class SampleClass
 {
     public async Task GetData()
     {
-        // Este método debería renombrarse a GetDataAsync
+        // deberia renombrarse a GetDataAsync
     }
     public void ProcessDto()
     {
-        // Dto debería cambiar a DTO
+        // deberia cambiar a DTO
     }
     public void SaveVm()
     {
-        // Vm debería cambiar a VM
+        // deberia cambiar a VM
     }
 }
